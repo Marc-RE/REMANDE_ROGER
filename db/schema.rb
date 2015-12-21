41f4bdc909a56717785f359889d092a9a86250b6
marc <marc@vpshark.io>
2015-12-21 22:19:24 +0100

    Database fixes due to errors in Weapon creation caused by misapplication of devise

--- db/schema.rb
+++ db/schema.rb
@@ -53,19 +53,6 @@ ActiveRecord::Schema.define(version: 20151221203738) do
     t.datetime "updated_at"
     t.string   "game"
     t.string   "video_game"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
   end
-
-  add_index "weapons", ["email"], name: "index_weapons_on_email", unique: true
-  add_index "weapons", ["reset_password_token"], name: "index_weapons_on_reset_password_token", unique: true
 
 end
